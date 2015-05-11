92c060e10fdbc496e7fa18111e78ebbe22ed2ceb
Thet Htut Oo <tho2410@gmail.com>
2015-05-11 09:50:58 +0000

    Edit Button added, Update not working

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -19,6 +19,10 @@ before_action :authenticate_user!, :only => [:new, :create]
 		    @place = Place.find(params[:id])
 	end
 	
+	def edit
+			@place = Place.find(params[:id])
+	end
+	
 	private
 	
 	def place_params
