eb1d8d542e412ca706f847ad3ec12bed409c539f
Thet Htut Oo <tho2410@gmail.com>
2015-05-11 03:24:04 +0000

    Forms are added

--- Gemfile
+++ Gemfile
@@ -46,3 +46,8 @@ end
 
 gem 'bootstrap-sass', '~> 3.3.0'
 gem 'autoprefixer-rails'
+
+gem 'will_paginate', '~> 3.0.7'
+gem 'will_paginate-bootstrap', '~> 1.0.1'
+
+gem 'simple_form'
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,9 @@
 class PlacesController < ApplicationController
 	def index
-		@places = Place.all 
+		@places = Place.all.order("created_at DESC").paginate(page: params[:page], per_page: 2) 
+	end
+	
+	def new
+		@place = Place.new
 	end
 end
