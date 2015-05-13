7dcce3677a863504e057663e63b4777b096801b2
Thet Htut Oo <tho2410@gmail.com>
2015-05-13 14:30:50 +0000

    Security measures added

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,13 @@ before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :des
 	end
 	
 	def create
-	    current_user.places.create(place_params)
-	    redirect_to root_path
+	    @place = current_user.places.create(place_params)
+	    if @place.valid?
+	    	
+	    	redirect_to root_path
+	    else
+	    	render :new, :status => :unprocessable_entity
+	    end
 	end
 	
 	
@@ -33,13 +38,19 @@ before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :des
 				return render :text => 'Not Allowed', :status => :forbidden
 			end
 			@place.update_attributes(place_params)
-			redirect_to root_path
+			if @place.valid?
+				redirect_to root_path
+			else
+				render :edit, status => :unprocessable_entity
+			end
+			
 	end
 	
 	def destroy
 		@place = Place.find(params[:id])
 		if @place.user != current_user
 			return render :text => 'Not Allowed', :status => :forbidden
+		end
 		@place.destroy
 		redirect_to root_path
 	end
